8ea946f0871af91cf7a62d8331fe9ddbc2ce391c
Karly1234 <98110883+Karly1234@users.noreply.github.com>
2023-02-09 13:13:47 -0600

    This is the first part of my new function

--- SoftwareEngineering.cpp
+++ SoftwareEngineering.cpp
@@ -22,6 +22,13 @@ int multofarray(std::vector <int> b){
     }
     return prod;
 }
+std::vector<int> reverse(std::vector <int> c){
+    std::vector<int> result;
+    for(int i =0 ; i < c.size(); i++){
+        int j = result[i];
+        result.push_back(j);
+    }
+}
 
 int main() {
 
@@ -34,7 +41,8 @@ int main() {
    }
 
     std::cout << sumofarray(vect) << '\n';
-    std::cout << multofarray(vect);
+    std::cout << multofarray(vect) << '\n';
+
 
     return 0;
 
